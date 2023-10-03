6a960aa1e4f1e572c641308adb413eb8380f9ac2
mounilKshah <mkshah_b18@ce.vjti.ac.in>
2023-10-03 07:23:54 +0530

    Socket code trial

--- go-socket-trial/go-sockets/server/main.go
+++ go-socket-trial/go-sockets/server/main.go
@@ -31,9 +31,85 @@ var colors = []string{
 var clients = make(map[string]net.Conn) // Map to store connections using UUID
 var mutex = &sync.Mutex{}
 
+func main() {
+	fmt.Println("Starting", connType, "server on", connHost+":"+connPort)
+
+	/**
+	l, err := net.Listen(connType, connHost+":"+connPort)
+	if err != nil {
+		log.Fatalf("Error listening: %v", err)
+	}
+	defer l.Close()
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			log.Fatalf("Error connecting: %v", err)
+		}
+		go handleConnection(c)
+	}
+	*/
+
+	ticker := time.NewTicker(2 * time.Second)
+
+	// Creating channel using make
+	tickerChan := make(chan bool)
+
+	l, err := net.Listen(connType, connHost+":"+connPort)
+	if err != nil {
+		log.Fatalf("Error listening: %v", err)
+	}
+	defer l.Close()
+
+	go func() {
+		for {
+			c, err := l.Accept()
+			if err != nil {
+				log.Fatalf("Error connecting: %v", err)
+			}
+			go handleConnection(c)
+		}
+		// for {
+		// 	select {
+		// 	case <-tickerChan:
+		// 		return
+		// 		// interval task
+		// 	case tm := <-ticker.C:
+		// 		fmt.Println("The Current time is: ", tm)
+		// 		// go intervalTask()
+
+		// 		for {
+		// 			c, err := l.Accept()
+		// 			if err != nil {
+		// 				log.Fatalf("Error connecting: %v", err)
+		// 			}
+		// 			go handleConnection(c)
+		// 		}
+		// 	}
+		// }
+	}()
+
+	// Calling Sleep() method
+	time.Sleep(10 * time.Second)
+
+	// Calling Stop() method
+	ticker.Stop()
+
+	// Setting the value of channel
+	tickerChan <- true
+
+	// Printed when the ticker is turned off
+	fmt.Println("Ticker is turned off!")
+}
+
+func intervalTask() {
+	fmt.Println("This is interval task")
+}
+
 func handleConnection(conn net.Conn) {
 	// Generate a UUID for the client
 	clientUUID := uuid.New().String()
+	fmt.Println("handle connection called")
 
 	// Pick a random color and send it to the client
 	rand.Seed(time.Now().UnixNano())
@@ -60,29 +136,12 @@ func handleConnection(conn net.Conn) {
 			fmt.Println("Client", clientUUID, "left.")
 			return
 		}
-
+		// message = "temp data"
 		// Broadcast the message to all clients except the sender
 		for id, c := range clients {
 			if id != clientUUID {
 				c.Write([]byte("[" + clientUUID + "]: " + message))
 			}
 		}
-	}
-}
-
-func main() {
-	fmt.Println("Starting", connType, "server on", connHost+":"+connPort)
-	l, err := net.Listen(connType, connHost+":"+connPort)
-	if err != nil {
-		log.Fatalf("Error listening: %v", err)
-	}
-	defer l.Close()
-
-	for {
-		c, err := l.Accept()
-		if err != nil {
-			log.Fatalf("Error connecting: %v", err)
-		}
-		go handleConnection(c)
 	}
 }
