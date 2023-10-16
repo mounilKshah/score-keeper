020a84ce844cf0ab12b4391a6f52d54187dc838f
mounilKshah <mkshah_b18@ce.vjti.ac.in>
2023-10-16 06:19:37 +0530

    main: Shifted to server with endpoints

--- rapid-api-rest/main.go
+++ rapid-api-rest/main.go
@@ -3,12 +3,38 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
+	// getTimezoneOld()
+	handleRequests()
+}
 
-	url := "https://v3.football.api-sports.io/{endpoint}"
+func handleRequests() {
+	// creates a new instance of a mux router
+	myRouter := mux.NewRouter().StrictSlash(true)
+	// replace http.HandleFunc with myRouter.HandleFunc
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/timezone", getTimezone)
+	// myRouter.HandleFunc("/all", returnAllArticles)
+	// myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+
+	// finally, instead of passing in nil, we want
+	// to pass in our newly created router as the second
+	// argument
+	log.Fatal(http.ListenAndServe(":9000", myRouter))
+}
+
+func getTimezone(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Body)
+	fmt.Println(r.URL)
+
+	url := "https://v3.football.api-sports.io/teams"
+	// url := "https://v3.football.api-sports.io/players/seasons"
 	method := "GET"
 
 	client := &http.Client{}
@@ -33,5 +59,15 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	fmt.Fprintf(w, "Timezone API hit")
+	// // fmt.Fprintf(w, string(body))
 	fmt.Println(string(body))
+	fmt.Println("Endpoint Hit: Timezone page")
 }
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}	
+
+
